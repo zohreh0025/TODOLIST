2367f921fb4da093e6ded4a115921d039ec9ebe2
zohrehAndroid <zohreh1455@gmail.com>
2020-03-22 05:26:58 -0700

    3 commit

--- app/src/main/java/todolist/ir/MainActivity.java
+++ app/src/main/java/todolist/ir/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmant_todo, new TodoListFragmant())
+                .commit();
     }
 }
